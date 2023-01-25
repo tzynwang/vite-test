f37e2fed46184d3da06fed80af69e46ae7362ed6
Tzu Yin <tzyn.wang@gmail.com>
2023-01-25 17:22:33 +0800

    [feat] use .env in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/vite-test',
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    base: '/vite-test',
+    server: {
+      open: true,
+      port: parseInt(env.PORT),
+    },
+    build: {
+      outDir: env.BUILD_OUTPUT,
+    },
+  };
 });
