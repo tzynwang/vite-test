f27205e5015decbd0b7a58d3afc73ebc793727ae
Tzu Yin <tzyn.wang@gmail.com>
2023-01-26 17:29:37 +0800

    [feat] add svgr to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd());
   console.info(path.resolve(__dirname, './src'));
   return {
-    plugins: [react()],
+    plugins: [react(), svgr()],
     base: env.VITE_BASE,
     css: {
       modules: {
