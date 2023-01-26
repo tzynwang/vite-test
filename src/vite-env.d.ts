41769b47f7ce764641be4c9eb10d67cf5df8bce8
Tzu Yin <tzyn.wang@gmail.com>
2023-01-26 17:30:10 +0800

    [feat] extend *.svg type declaration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+declare module '*.svg' {
+  import * as React from 'react';
+
+  const src: string;
+
+  export default src;
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+}
