1f5330e800fafa1a15bed4504769532623b699d9
Tzu Yin <tzyn.wang@gmail.com>
2023-02-02 21:37:28 +0800

    [feat] add Snippet page

--- src/components/Page/Snippet/index.tsx
+++ src/components/Page/Snippet/index.tsx
@@ -1,11 +1,60 @@
 import React, { memo } from 'react';
+import cn from 'classnames';
+import CategoryList from '@/components/Common/CategoryList';
+import Code from '@/components/Common/Code';
+import Breadcrumbs from '@/components/Layout/Breadcrumbs';
 import ContentListLayout from '@/components/Layout/ContentList';
+import PostListContainer from '@/components/Layout/PostListContainer';
+import {
+  MOCK_POST_CATEGORY_PAIRS_2,
+  MOCK_CSS_CODE,
+  MOCK_JSON_CODE,
+  MOCK_MAKEFILE_CODE,
+} from '@/models/GeneralModels';
+import scopedStyles from './index.module.css';
 
 function Snippet(): React.ReactElement {
+  /* Main */
   return (
     <ContentListLayout
-      side={<React.Fragment>snippet side content</React.Fragment>}
-      main={<React.Fragment>snippet main content</React.Fragment>}
+      side={<CategoryList categories={MOCK_POST_CATEGORY_PAIRS_2} />}
+      main={
+        <PostListContainer>
+          <div>
+            <Breadcrumbs>
+              <span>category</span>
+              <span>snippet title</span>
+            </Breadcrumbs>
+            <p>time</p>
+            <p>snippet description</p>
+            <div className={cn(scopedStyles.scopedStyles)}>
+              <Code language="css">{MOCK_CSS_CODE}</Code>
+            </div>
+          </div>
+          <div>
+            <Breadcrumbs>
+              <span>category</span>
+              <span>snippet title</span>
+            </Breadcrumbs>
+            <p>time</p>
+            <p>snippet description</p>
+            <div className={cn(scopedStyles.scopedStyles)}>
+              <Code language="json">{MOCK_JSON_CODE}</Code>
+            </div>
+          </div>
+          <div>
+            <Breadcrumbs>
+              <span>category</span>
+              <span>snippet title</span>
+            </Breadcrumbs>
+            <p>time</p>
+            <p>snippet description</p>
+            <div className={cn(scopedStyles.scopedStyles)}>
+              <Code language="makefile">{MOCK_MAKEFILE_CODE}</Code>
+            </div>
+          </div>
+        </PostListContainer>
+      }
     />
   );
 }
